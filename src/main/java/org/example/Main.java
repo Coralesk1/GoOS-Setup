bede9b098d0e65690af42c13405d10f1d94a6140
Coral <guscoraleski2006@gmail.com>
2026-04-26 22:03:49 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carrega o arquivo de layout
+        Parent root = FXMLLoader.load(getClass().getResource("/view/TelaPrincipal.fxml"));
+
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+/*
+* - extends é para que o Main se torna uma aplicação grafica
+* - No Main o metodo launch() é responsavel por inicializar o ambiemte JavaFX , e chama internamente o metodo start()
+* - o metodo start() é onde tudo começa , pois é ela que defini a janela
+* - FXMLLoader esse comando le o que tem no (fxml) e transforma em um objeto java Parent
+* - Scene é o conteudo da janela
+* - primaryStage.show(): Por padrão, a janela é criada invisível. Esse comando a torna visível para o usuário.
+* */
