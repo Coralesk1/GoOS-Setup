bce5b81830977845151ba32fedb3d311caa1fb69
Coral <guscoraleski2006@gmail.com>
2026-05-02 00:28:16 -0300

    vendo como que eu vou faz para salvar as info das telas.

--- src/main/java/org/example/controller/WelcomeController.java
+++ src/main/java/org/example/controller/WelcomeController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 
 public class WelcomeController {
 
@@ -10,7 +12,28 @@ public class WelcomeController {
     private Button btnNext;
 
     @FXML
+    private ListView<String> listLinguagens;
+
+
+
+    @FXML
     void handleNext(ActionEvent event) {
         Navigation.navigate("/view/TelaTimeZone.fxml");
     }
+
+    @FXML
+    public void initialize(){
+        listLinguagens.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                System.out.println("Linguagem selecionada: " + newValue);
+            }
+        });
+
+    }
+
+
+
+
 }
+
+
